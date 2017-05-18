07d5231149d7732e2973ec5b5b7146a9b97b79c4
Bala Chirtsabesan <balaTest@live.com>
2017-05-18 14:00:58 -0700

    [WebApp_Anc20_core_NoAuth_1] Added BlogsController with Index and Create views

---
+++ Dev15.3Preview1_Cli2.0Preview1/WebApp_Anc20_core_NoAuth_1/WebApp_Anc20_core_NoAuth_1/Models/BloggingContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp_Anc20_core_NoAuth_1.Models
+{
+    public class BloggingContextFactory : IDbContextFactory<BloggingContext>
+    {
+        public BloggingContext Create(string[] args) =>
+            Program.BuildWebHost(args).Services.GetRequiredService<BloggingContext>();
+    }
+
+}
