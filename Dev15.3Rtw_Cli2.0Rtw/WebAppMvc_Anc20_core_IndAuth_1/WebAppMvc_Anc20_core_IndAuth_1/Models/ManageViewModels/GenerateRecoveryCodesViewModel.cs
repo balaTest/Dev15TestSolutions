167336f6caa91281b2efa8a2bc52e505c4732005
Bala Chirtsabesan <balaTest@live.com>
2017-09-08 14:28:05 -0700

    [WebAppMvc_Anc20_core_IndAuth_1] Created solution using Dev15.3.3 + CLI 2.0

---
+++ Dev15.3Rtw_Cli2.0Rtw/WebAppMvc_Anc20_core_IndAuth_1/WebAppMvc_Anc20_core_IndAuth_1/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMvc_Anc20_core_IndAuth_1.Models.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
