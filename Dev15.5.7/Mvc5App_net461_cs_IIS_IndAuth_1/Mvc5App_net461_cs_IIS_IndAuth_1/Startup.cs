ad5ed16f4e9fe9cb29f99a7db308b1cfa4276d14
Bala Chirtsabesan <balaTest@live.com>
2018-03-28 06:45:00 -0700

    [Mvc5App_net461_cs_IIS_IndAuth_1] Created solution with IISExpress as the web server

---
+++ Dev15.5.7/Mvc5App_net461_cs_IIS_IndAuth_1/Mvc5App_net461_cs_IIS_IndAuth_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5App_net461_cs_IIS_IndAuth_1.Startup))]
+namespace Mvc5App_net461_cs_IIS_IndAuth_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
