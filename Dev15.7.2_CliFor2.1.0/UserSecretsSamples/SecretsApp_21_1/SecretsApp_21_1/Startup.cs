82346d1866ea4a47d9be2f560d4ee47c4c9fcdd6
Bala Chirtsabesan <balaTest@live.com>
2018-06-12 07:53:05 -0700

    [SecretsApp_21_1] Retrieve and display a secret

--- Dev15.7.2_CliFor2.1.0/UserSecretsSamples/SecretsApp_21_1/SecretsApp_21_1/Startup.cs
+++ Dev15.7.2_CliFor2.1.0/UserSecretsSamples/SecretsApp_21_1/SecretsApp_21_1/Startup.cs
@@ -5,16 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SecretsApp_21_1
 {
     public class Startup
     {
+        private string _mySecret = null;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            _mySecret = Configuration["myAppSetting"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +36,12 @@ namespace SecretsApp_21_1
                 app.UseDeveloperExceptionPage();
             }
 
+            var result = string.IsNullOrEmpty(_mySecret) ? "Null" : _mySecret;
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync($"Secret is {result}");
             });
+
         }
     }
 }
