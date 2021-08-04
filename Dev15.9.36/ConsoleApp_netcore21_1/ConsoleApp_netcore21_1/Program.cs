52922a1e81f7d4c4429fc654fdfdaef74ba43df0
Bala Chirtsabesan <balatest@live.com>
2021-08-03 23:43:31 -0700

    [ConsoleApp_netcore21_1] Created solution

---
+++ Dev15.9.36/ConsoleApp_netcore21_1/ConsoleApp_netcore21_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_netcore21_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
